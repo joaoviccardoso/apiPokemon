b0714e1a2032a3e7394942533760beaee17c110f
joao victor da silva cardoso <pealq63@gmail.com>
2024-09-18 23:59:35 -0300

    foi pego a pokeapi com a getApi, com a funcao pokemons foi utilizada  mostrar apenas 150 pokemons, craiarCardPokemon e usada para craiar os card e foi configurado o layaud do aside esquerdo e direito deixando eles fixed na tela

--- js/api.js
+++ js/api.js
@@ -1,6 +1,32 @@
-async function getApi(){
-    const respostaApi = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`);
-    const listaPokemons = await respostaApi.json()
-    console.log(listaPokemons)
+const containerPokemons = document.querySelector('.lista__pokedex');
+let contadorDePokemons = 150;
+
+async function pokemons() {
+    for (let i = 1; i <= contadorDePokemons; i++) {
+        await getApi(i)
+    }
 }
 
+async function getApi(id){
+    const respostaApi = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const listaPokemons = await respostaApi.json()
+    criarCardPokemon(listaPokemons)
+    console.log(listaPokemons.name)
+}
+
+function criarCardPokemon(pokemon){
+    containerPokemons.innerHTML += `
+        <li class="lista_pokemon">
+            <div class="card" style="width: 12rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${pokemon.name}</h5>
+                    <p class="card-text">${pokemon.id.toString()}</p>
+                    <p class="card-text">${pokemon.types.map(type => type.type.name)}</p>
+                </div>
+            </div>
+        </li>
+    `
+}
+
+pokemons()
