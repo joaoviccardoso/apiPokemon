c45de39621c7991611260066a81d663453111cd2
joao victor da silva cardoso <pealq63@gmail.com>
2024-09-21 00:49:29 -0300

    foi mexido no layaud e foi pego os pokemons e colocados em uma lista

--- js/api.js
+++ js/api.js
@@ -1,6 +1,9 @@
 const containerPokemons = document.querySelector('.lista__pokedex');
-let contadorDePokemons = 151;
 
+let contadorDePokemons = 151;
+let listaDosPokemons = [];
+
+console.log(listaDosPokemons)
 async function pokemons() {
     for (let i = 1; i <= contadorDePokemons; i++) {
         await getApi(i)
@@ -10,19 +13,20 @@ async function pokemons() {
 async function getApi(id){
     const respostaApi = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const listaPokemons = await respostaApi.json()
+    listaDosPokemons.push(listaPokemons)
     criarCardPokemon(listaPokemons)
-    console.log(listaPokemons)
 }
 
 function criarCardPokemon(pokemon){
     containerPokemons.innerHTML += `
         <li class="lista__pokemon">
             <div class="card">
-                <img src="${pokemon.sprites.front_default}" class="card-img-top" alt="${pokemon.name}">
+                <img src="${pokemon.sprites.front_default}" class="card-img-top img-card-pokemon" alt="${pokemon.name}">
                 <div class="card-body">
-                    <h5 class="card-title">${pokemon.name}</h5>
-                    <p class="card-text">${pokemon.id.toString()}</p>
+                    <p class="card-text">#${pokemon.id.toString()}</p>
+                    <h5 class="card-title">${pokemon.name[0].toUpperCase() + pokemon.name.substring(1)}</h5>
                     <p class="card-text">${pokemon.types.map(type => type.type.name)}</p>
+                    <button type="button" class="btn btn-success">Saber mais</button>
                 </div>
             </div>
         </li>
