2cd60330f96cfee82a20d15b88d2e5530f9e8cb9
joao victor da silva cardoso <pealq63@gmail.com>
2024-09-25 18:16:37 -0300

    foi terminado o card direito adicionando estilo e o audio do pokemon

--- js/saberMais.js
+++ js/saberMais.js
@@ -8,7 +8,6 @@ function pegarBtnsSaberMais(){
             const idDoBtn = this.id
             const idDoBtnString = idDoBtn.split("-")[1];
             const numeroDoIdDoBtn = Number(idDoBtnString) - 1;
-            console.log(numeroDoIdDoBtn)
             saberMaisSobrePokemon(numeroDoIdDoBtn);
         })
     })
@@ -18,20 +17,34 @@ function saberMaisSobrePokemon(numeroDoIdDoBtn){
     console.log(listaDosPokemons[numeroDoIdDoBtn])
     containerSaberMais.innerHTML = `
         <div class="card cardStatus">
-            <img src="${listaDosPokemons[numeroDoIdDoBtn].sprites.other.showdown.front_default}" class="card-img-top" alt="${listaDosPokemons[numeroDoIdDoBtn].name}">
+            <img src="${listaDosPokemons[numeroDoIdDoBtn].sprites.other.showdown.front_default}" class="card-img-top img-saberMais" alt="${listaDosPokemons[numeroDoIdDoBtn].name}">
             <div class="card-body">
               <h5 class="card-title">${listaDosPokemons[numeroDoIdDoBtn].name}</h5>
-              <p class="card-text"></p>
+              <p class="card-text">${listaDosPokemons[numeroDoIdDoBtn].types.map(type => type.type.name)}</p>
             </div>
             <ul class="list-group list-group-flush">
               <li class="list-group-item">Peso: ${listaDosPokemons[numeroDoIdDoBtn].weight / 10}Kg</li>
               <li class="list-group-item">Altura: ${listaDosPokemons[numeroDoIdDoBtn].height / 10}m</li>
-              <li class="list-group-item">A third item</li>
             </ul>
             <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
+              <button type="button" id="botaoSaberMais-${listaDosPokemons[numeroDoIdDoBtn].id}" class="btn btn-saberMais ">Som do Pokemon</button>
             </div>
           </div>
     `
+    btnSaberMaisPokemon()      
+}
+
+function btnSaberMaisPokemon(){
+  const btnSaberMais = document.querySelector(".btn-saberMais"); 
+  btnSaberMais.addEventListener('click', function() {
+    const idBtnSaberMais = this.id;
+    const idBtnSaberMaisString = idBtnSaberMais.split("-")[1];
+    const idBtnSaberMaisNumero = Number(idBtnSaberMaisString) - 1;
+    somDoPokemon(idBtnSaberMaisNumero)
+  })
+}
+
+function somDoPokemon(id){
+  const audio = new Audio(`${listaDosPokemons[id].cries.latest}`)
+  audio.play()
 }
