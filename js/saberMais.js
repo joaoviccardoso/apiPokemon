cd9e0e8358fee4d8331bbfd9dafeaea97e53e0a1
seu nome de usuário <pealq63@gmail.com>
2025-01-22 18:53:39 -0300

    Criado arquivo buttons.css para organizar os estilos dos botões - Removido fundo com imagens de Pokémon para um gradiente azul - Melhorado o design para manter uma aparência mais limpa e profissional

--- js/saberMais.js
+++ js/saberMais.js
@@ -63,8 +63,7 @@ function saberMaisSobrePokemon(numeroDoIdDoBtn){
                 </li>
             </ul>
             <div class="card-body">
-              <button type="button" id="botaoSaberMais-${listaDosPokemons[numeroDoIdDoBtn].id}" class="btn-saberMais botao d-none d-sm-block">Som do Pokemon</button>
-              <button type="button" id="botaoSomCelular-${listaDosPokemons[numeroDoIdDoBtn].id}" class="btn btnSomCelular d-sm-none">Som</button>
+              <button type="button" id="botaoSaberMais-${listaDosPokemons[numeroDoIdDoBtn].id}" class="btn-saberMais botao">Som do Pokemon</button>
             </div>
         </div>
     `;
