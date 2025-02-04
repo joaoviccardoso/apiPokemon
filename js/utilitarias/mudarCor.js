622fe81bac26931a2a0def9599fe3c1dadf09f5d
seu nome de usuário <pealq63@gmail.com>
2025-02-04 20:32:24 -0300

    Correção na troca de tema e ajustes nas cores para azul e vermelho inspirados em Pokémon.

--- js/utilitarias/mudarCor.js
+++ js/utilitarias/mudarCor.js
@@ -3,11 +3,11 @@ import { thisIsDarkModel } from "../app.js";
 export function verificarBtnThema(){
    // Verifica o estado atual e alterna entre tema claro e escuro
       if(thisIsDarkModel.tema){
-         // Se já estiver no modo escuro, muda para o modo claro
+         // Se já estiver no modo vermelho, muda para o modo azul
          console.log("verdade")
          thisIsDarkModel.tema = false;
       } else {
-         // Se estiver no modo claro, muda para o modo escuro
+         // Se estiver no modo azul, muda para o modo 
          console.log("falso")
          thisIsDarkModel.tema = true;
       }
@@ -20,9 +20,11 @@ export function verificarBtnThema(){
 // Função que carrega o tema baseado no valor de thisIsDarkModel
 function chargeTheTheme(){
    // Se estiver no modo escuro, aplica o tema escuro e troca a imagem para o ícone de sol
-   if(thisIsDarkModel){
+   if(thisIsDarkModel.tema){
+      console.log("verdade")
       blackTheme();  // Aplica o tema escuro
    } else {
+      console.log("falso")
       // Se estiver no modo claro, aplica o tema claro e troca a imagem para o ícone de lua
       whiteTheme();  // Aplica o tema claro
    }
@@ -31,17 +33,11 @@ function chargeTheTheme(){
 // Função que aplica o tema claro, alterando as variáveis CSS (cores)
 function whiteTheme(){
    // Define as cores para o tema claro usando variáveis CSS
-   document.documentElement.style.setProperty('--backgroud', '#fcf8ff');  // Cor de fundo clara
-   document.documentElement.style.setProperty('--card', '#fff');  // Cor do card (branco)
-   document.documentElement.style.setProperty('--title', '#000');  // Cor do título (preto)
-   document.documentElement.style.setProperty('--border', '#ccc');  // Cor da borda (cinza claro)
+   document.documentElement.style.setProperty('--backgroud', 'linear-gradient(to bottom, #5bc0f8, #1c1c3c)');  // Cor de fundo clara
 }
 
 // Função que aplica o tema escuro, alterando as variáveis CSS (cores)
 function blackTheme(){
    // Define as cores para o tema escuro usando variáveis CSS (referências para variáveis já definidas no CSS)
-   document.documentElement.style.setProperty('--backgroud', 'var(--background-dark)');  // Cor de fundo escura
-   document.documentElement.style.setProperty('--card', 'var(--card-dark)');  // Cor do card escuro
-   document.documentElement.style.setProperty('--title', 'var(--title-black)');  // Cor do título para tema escuro
-   document.documentElement.style.setProperty('--border', 'var(--card-dark)');  // Cor da borda (escuro)
+   document.documentElement.style.setProperty('--backgroud', 'linear-gradient(to bottom, #E63946, #900C3F)');  // Cor de fundo escura
 }
