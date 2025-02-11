bf461c5c9072f42aaf39c32f8beeeeac8385ad94
seu nome de usuário <pealq63@gmail.com>
2025-02-11 20:48:41 -0300

    foi pego os level dos pokemons e colocado no array

--- js/utilitarias/extrairIdPokemon.js
+++ js/utilitarias/extrairIdPokemon.js
@@ -1,12 +1,16 @@
 export function extrairEvolucoes(chain) {
     const evolucoes = [];
+    const niveis = [];
 
     function processarEvolucao(etapa) {
         const nome = etapa.species.name;
         const id = etapa.species.url.split("/").slice(-2, -1)[0]; //pega o id do pokemon
         const imagem = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
 
-        evolucoes.push({ nome, imagem });
+        // Pega o nível mínimo de evolução, se existir
+        const minLevel = etapa.evolution_details.length > 0 ? etapa.evolution_details[0].min_level : null;
+
+        evolucoes.push({ nome, imagem, minLevel });
 
         if (etapa.evolves_to.length > 0) {
             etapa.evolves_to.forEach(processarEvolucao);
